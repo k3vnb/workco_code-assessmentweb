ceca4155cc54d798477db68c419357c150e01542
Kevin Boyle <papershack@gmail.com>
2019-03-10 15:29:03 -0700

    add structure to product cards to correspond w/ Sketch mock-up. Begin styling for mobile view

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,13 +3,34 @@ import PropTypes from 'prop-types'
 
 const Product = ({ price, inventory, title, thumbnail }) => (
   <div className="product-card-top">
-    <div><img className="thumbnail" src={thumbnail} alt="product thumbnail"/></div>
-    {title} - &#36;{price}{inventory ? ` x ${inventory}` : null}
+    <div>
+      <img className="thumbnail" src={thumbnail} alt="product thumbnail"/>
+    </div>
+    <div className="product-name-and-price">
+      <div className="product-name">
+        {title}
+      </div>
+      <div className="product-price">
+        {price}
+      </div>
+    </div>
+    <div className="inventory-count">
+      {inventory ? `${inventory} remaining` : `Sold Out!`}
+    </div>
     <style jsx>{`
       .thumbnail{
         object-fit: contain;
         height: 50px;
         width: 50px;
+      }
+      .product-card-top {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+      .product-name-and-price {
+        display: flex;
+        justify-content: space-between;
       }
     `}</style>
   </div>
